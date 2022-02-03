558607b011c9aa4de283aacc5eed19fbe9b327b3
seogiwon <seokw2240@gmail.com>
2022-02-03 22:47:22 +0900

    start project

---
+++ app/src/main/java/com/bcsd/android/lotteryticketapplication/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bcsd.android.lotteryticketapplication.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bcsd.android.lotteryticketapplication.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
