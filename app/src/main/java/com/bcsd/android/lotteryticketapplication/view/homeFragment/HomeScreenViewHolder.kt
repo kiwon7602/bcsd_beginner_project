e7af7dcdb22a895d0c384421142efc3109f9eb91
Jokwanhee <jonotch1@naver.com>
2022-02-23 22:00:35 +0900

    Add all data version1.01

--- app/src/main/java/com/bcsd/android/lotteryticketapplication/view/homeFragment/HomeScreenViewHolder.kt
+++
@@ -1,9 +0,0 @@
-package com.bcsd.android.lotteryticketapplication.view.homeFragment
-
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-
-class HomeScreenViewHolder(itemView: View) :RecyclerView.ViewHolder(itemView){
-
-
-}
