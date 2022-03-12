0db7a47188ddbee4bf2af49eda47499f37956747
Seungmin Yang <tmdals099@gmail.com>
2022-03-12 21:26:41 +0900

    Revert "Bugfix/mypage userview"

--- app/src/main/java/com/bcsd/android/lotteryticketapplication/view/model/LotteryNumber.kt
+++
@@ -1,9 +0,0 @@
-package com.bcsd.android.lotteryticketapplication.view.model
-
-import com.google.gson.annotations.SerializedName
-
-// 당첨 로또 번호와 당첨 날짜를 가지고 있는 데이터 클래스
-data class LotteryNumber(
-    @SerializedName("lotto_numbers") val lottoNumbers:ArrayList<Int>,
-    @SerializedName("date") val date:String
-)
